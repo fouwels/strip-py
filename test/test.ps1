193065f56b7634b419c5a283d8daae34e15a3970
Kaelan Fouwels <kfouwels@outlook.com>
2015-08-01 19:36:30 +0100

    really shouldn#t have been that hard pshell

--- test/test.ps1
+++ test/test.ps1
@@ -1,3 +1,8 @@
 $e = (curl.exe -H "Content-Type: multipart/form-data" -F "image=@clear.png" localhost:5000)
-$decoded = ([System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($e)))
-$decoded | Out-File("out.png")
+
+echo "Raw: " $e
+echo ""
+$decoded = [System.Convert]::FromBase64CharArray($e, 0, $e.Length)
+[io.file]::WriteAllBytes('out.png', $decoded)
+
+Get-Content('out.png') | Format-Hex
